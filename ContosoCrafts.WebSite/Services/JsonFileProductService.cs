4ffe7b811aca65f6d73404bdcd8c684df9b8bef4
unknown <alexandru.turcu@SVROLP01666.ro.softvision.global>
2020-09-24 09:53:21 +0300

    Initial commit

---
+++ ContosoCrafts.WebSite/Services/JsonFileProductService.cs
@@ -0,0 +1,60 @@
+﻿namespace ContosoCrafts.WebSite.Services
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.Json;
+    using Microsoft.AspNetCore.Hosting;
+    using Models;
+
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        private string JsonFileName => Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json");
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonSerializer = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonSerializer.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public void AddRating(string productId, int rating)
+        {
+            var products = GetProducts();
+            var query = products.First(x => x.Id == productId);
+
+            if (query.Ratings == null)
+            {
+                query.Ratings = new [] {rating};
+            }
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize(new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                {
+                    SkipValidation = true,
+                    Indented = true
+                }),
+                    products);
+            }
+        }
+    }
+}
